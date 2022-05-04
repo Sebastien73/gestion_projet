f7676a6aea0749e2869191d6cf3aa7e068d5af80
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-05-04 11:39:44 +0200

    Add LoRa images

---
+++ src/components/header/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+            <b-navbar-nav>
+                <b-nav-item href="https://le-campus-numerique.fr/">
+                    <img src="https://le-campus-numerique.fr/wp-content/uploads/2020/12/logo-campus-header.png" alt="">
+                </b-nav-item>
+
+                <!-- Navbar dropdowns -->
+                <b-nav-item-dropdown text="Lang" right>
+                    <b-dropdown-item href="#">EN</b-dropdown-item>
+                    <b-dropdown-item href="#">ES</b-dropdown-item>
+                    <b-dropdown-item href="#">RU</b-dropdown-item>
+                    <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown text="User" right>
+                    <b-dropdown-item href="#">Account</b-dropdown-item>
+                    <b-dropdown-item href="#">Settings</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
