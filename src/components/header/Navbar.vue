f7ed3b48a0026f7b3fd5f2b977d898ad0627987d
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-05-04 14:09:05 +0200

    Create Navbar.vue Component

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+            <b-navbar-nav>
+                <b-nav-item>
+                    <router-link to="/">
+                        <img src="https://le-campus-numerique.fr/wp-content/uploads/2020/12/logo-campus-header.png" alt="">
+                    </router-link>
+                </b-nav-item>
+
+                <b-nav-item>
+                    <router-link to="/electronique">Electronique</router-link>
+                    <router-link to="/protocoles">Protocoles de communications</router-link>
+                    <router-link to="/cas_etudes">Cas d'études</router-link>
+                </b-nav-item>
+
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
