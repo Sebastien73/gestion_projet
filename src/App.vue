00d57f466303d9362120d520372fdfe9bff141bf
Seb <jssebastienjacquelin@hotmail.com>
2022-05-05 13:43:05 +0200

    add background-color for app

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-
+  color: black;
+  background-color: white;
 }
 
 footer {
