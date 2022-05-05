17f86554b4182538471add42bb3b8e3d7487fc00
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-05-05 12:17:48 +0200

    Navbar fixed & WIP header images

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,5 +1,7 @@
 <template>
     <Navbar />
+
+<!--    <img v-if=" router page === image.title" class="img-fluid" :src={{images.url }} :alt="{{ images.alt[title] }}" :key="{{ images.title }}">-->
 </template>
 
 <script>
@@ -8,9 +10,32 @@ export default {
   // eslint-disable-next-line vue/multi-word-component-names
     name: "Header",
     components: {Navbar},
+
+    // data: function () {
+    //     return {
+    //         images: [
+    //             {},
+    //             {title: "protocoles", url: "../../assets/images/protocole/LoRA01.jp", alt: "Communication protocols image header"},
+    //             {title: "etude", url: "../../assets/images/etude/etude02.jpg", alt: "Study case image header"},
+    //             {title: "iot", url : "../../assets/images/etude/etude02.jpg", alt: "IoT image header"}
+    //         ]
+    //     }
+    // },
+    props: {
+        image1 : { title: "electronic", url: "../../assets/images/electronique/electronique06.webp", alt: "Electronic image header" },
+        image2 : {title: "etude", url: "../../assets/images/etude/etude02.jpg", alt: "Study case image header"},
+        image3 : {title: "iot", url : "../../assets/images/etude/etude02.jpg", alt: "IoT image header"}
+    }
 }
+
+
 </script>
 
 <style scoped>
+
+/*img {*/
+/*    margin-top: 100px;*/
+/*    height: 80vh;*/
+/*}*/
 
 </style>
