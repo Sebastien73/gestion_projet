f8e34386325c2526398e41911aea8d4c7c951875
Seb <jssebastienjacquelin@hotmail.com>
2022-05-06 09:05:43 +0200

    carousel on page cas_etudes

--- src/views/Cas_etudes.vue
+++ src/views/Cas_etudes.vue
@@ -137,6 +137,7 @@
         </div>
       </div>
     </div>
+    <Carousel_etudes/>
     <div class="d-flex justify-content-between my-5">
       <router-link to="/protocoles" class="btn btn-rose m-2"># PROTOCOLE DE COMMUNICATION <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
@@ -148,15 +149,16 @@
       </router-link>
     </div>
   </div>
+
 </div>
 
 </template>
 
 <script>
-// import Carousel_etudes from "@/components/carrousel_component/Carousel_etudes";
+import Carousel_etudes from "@/components/carrousel_component/Carousel_etudes";
 export default {
   name: "Cas_etudes",
-  // components: {Carousel_etudes}
+  components: {Carousel_etudes},
   data(){
     return{
       controlBlock: false
