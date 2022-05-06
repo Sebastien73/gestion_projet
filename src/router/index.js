176db065d596054003aec0863af62104f05cdac1
Seb <jssebastienjacquelin@hotmail.com>
2022-05-06 11:54:46 +0200

    test something for prod

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeIoT from '../views/HomeIoT.vue'
 import Electronique from '../views/Electronique.vue'
 import Protocoles_comm from '../views/Protocoles_comm.vue'
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
