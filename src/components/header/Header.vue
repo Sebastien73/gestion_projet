d5f612e4411b1a439cba3cf387593378795653fe
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-05-06 09:57:46 +0200

    Clean Header.vue

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -8,23 +8,7 @@ import Navbar from "@/components/header/Navbar";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
     name: "Header",
-    components: {Navbar},
-
-    // data: function () {
-    //     return {
-    //         images: [
-    //             {},
-    //             {title: "protocoles", url: "../../assets/images/protocole/LoRA01.jp", alt: "Communication protocols image header"},
-    //             {title: "etude", url: "../../assets/images/etude/etude02.jpg", alt: "Study case image header"},
-    //             {title: "iot", url : "../../assets/images/etude/etude02.jpg", alt: "IoT image header"}
-    //         ]
-    //     }
-    // },
-    props: {
-        image1 : { title: "electronic", url: "../../assets/images/electronique/electronique06.webp", alt: "Electronic image header" },
-        image2 : {title: "etude", url: "../../assets/images/etude/etude02.jpg", alt: "Study case image header"},
-        image3 : {title: "iot", url : "../../assets/images/etude/etude02.jpg", alt: "IoT image header"}
-    }
+    components: {Navbar}
 }
 
 
