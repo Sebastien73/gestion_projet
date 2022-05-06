4bcd735a524fe09f77f85623198e42a80774da77
Seb <jssebastienjacquelin@hotmail.com>
2022-05-06 14:47:56 +0200

    fix menu

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -1,25 +1,39 @@
 <template>
-    <div>
+  <div class="navbar-nav navbar-dark sticky-top navbar-expand-lg bg-dark justify-content-center align-items-center text-uppercase w-100">
+    <button id="btnnavmobile" class="btn bg-dark" @click="openMenu()">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    </button>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-uppercase w-100">
-            <a class="mr-md-3 navbar-header container" @click="scrollT"  href="/formation.htm">
-                <img id="logoCampus" class="img-fluid" src="https://le-campus-numerique.fr/wp-content/uploads/2020/12/logo-campus-header.png" alt="Logo Campus" width="">
-            </a>
-            <button id="burger" class="navbar-toggler" type="button" data-toggle="collapse"
-                           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                           aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse container" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active" v-for="(page) in pages" :key="page.id" >
-                        <router-link class="m-3 nav-link" @click="scrollT" :to="page.path"> <span class="sr-only"> {{ page.pageTitle }} </span> </router-link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <!--    Menu desktop-->
+    <div class="navbar-nav flex justify-content-center align-items-center" id="desktop">
+      <div class="d-flex">
+        <button id="btnnavdesktop" class="btn bg-dark">
+          <a class="mr-md-3 navbar-header container" @click="scrollT"  href="/formation.htm">
+            <img id="logoCampus" class="img-fluid" src="https://le-campus-numerique.fr/wp-content/uploads/2020/12/logo-campus-header.png" alt="Logo Campus" width="">
+          </a>
+        </button>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active" v-for="(page) in pages" :key="page.id" >
+            <router-link class="m-3 nav-link" @click="scrollT" :to="page.path"> <span class="sr-only"> {{ page.pageTitle }} </span> </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
+
+    <!--    Menu mobile-->
+    <div class="navbar-nav flex justify-content-center align-items-center" id="mobile" v-show="controlMenu">
+      <a class="mr-md-3 navbar-header container" @click="closeMenu"  href="/formation.htm">
+        <img id="logoCampus" class="img-fluid mt-3" src="https://le-campus-numerique.fr/wp-content/uploads/2020/12/logo-campus-header.png" alt="Logo Campus" width="">
+      </a>
+      <ul class="navbar-nav d-flex flex-column justify-content-center align-items-center">
+        <li class="nav-item active" v-for="(page) in pages" :key="page.id" >
+          <router-link class="m-3 nav-link" @click="closeMenu" :to="page.path"> <span class="sr-only"> {{ page.pageTitle }} </span> </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,20 +41,28 @@ export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Navbar",
     data() {
-        return {
-            pages: [
-                {pageTitle: "#iot", id: 1, path: "/home"},
-                {pageTitle: "#électronique", id: 2, path: "/electronique"},
-                {pageTitle: "#protocoles de communication", id: 3, path: "/protocoles"},
-                {pageTitle: "#cas d'études iot", id: 4, path: "/cas_etudes"}
-            ],
-        }
+      return {
+        pages: [
+          {pageTitle: "#iot", id: 1, path: "/home",},
+          {pageTitle: "#électronique", id: 2, path: "/electronique"},
+          {pageTitle: "#protocoles de communication", id: 3, path: "/protocoles"},
+          {pageTitle: "#cas d'études iot", id: 4, path: "/cas_etudes"}
+        ],
+        controlMenu : false
+      }
     },
 
     methods: {
-        scrollT(){
-            window.scroll(0,0);
-        }
+      scrollT(){
+        window.scroll(0,0);
+      },
+      openMenu(){
+        this.controlMenu = !this.controlMenu;
+      },
+      closeMenu(){
+        this.controlMenu = !this.controlMenu;
+        window.scroll(0,0);
+      }
     }
 }
 </script>
@@ -53,22 +75,22 @@ export default {
     z-index: 100;
 }
 
-.navbar .navbar-nav .nav-item .nav-link {
+.navbar-nav .nav-item .nav-link {
     font-family: "Open Sans", Sans-serif;
     font-weight: bold;
     color: #EF2961;
 }
 
 
-.navbar .navbar-nav .nav-item .nav-link:hover {
+.navbar-nav .nav-item .nav-link:hover {
     color: #bb0036;
 }
 
-.navbar .navbar-nav .nav-item .nav-link.active {
+.navbar-nav .nav-item .nav-link.active {
     color: white;
 }
 
-nav a.router-link-exact-active {
+a.router-link-exact-active {
   color: #FFFFFF !important;
 }
 
@@ -76,10 +98,44 @@ nav a.router-link-exact-active {
 #logoCampus {
     max-width: 40%;
 }
-
-/*__Menu_Burger_____________________*/
-#burger {
-
+ul{
+  list-style: none;
 }
-
+@media (min-width: 300px) and (max-width: 1024px) {
+  #desktop{
+    display: none;
+  }
+  #mobile{
+    display:block;
+  }
+  ul{
+    padding: 5px;
+  }
+  li{
+    margin: 5px;
+  }
+  #btnnavmobile{
+    width: 50%;
+    display: block;
+    color: #FFFFFF;
+  }
+  #btnnavdesktop{
+    display: none;
+  }
+}
+@media (min-width: 1025px){
+  #desktop{
+    display: block;
+  }
+  #mobile{
+    display: none;
+  }
+  #btnnavmobile{
+    display: none;
+  }
+  #btnnavdesktop{
+    width: 50%;
+    display: block;
+  }
+}
 </style>
